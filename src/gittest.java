6a9a0d0f0f4a2a6cc65398e69ce6a50d17b351fb
sidam <sidam@LAPTOP-D1IVGNI5>
2020-12-08 20:13:16 -0500

    ADDED SOURCE CODE WITH METHOD MAIN

--- src/gittest.java
+++ src/gittest.java
@@ -1,4 +1,12 @@
 
 public class gittest {
+	public static void main(String args[]) {
+		int[] primes = {2,3,5,7};
+		for(int index = 0; index <= primes.length;index++) {
+			System.out.println(primes[index]);
+	
+		}
+		System.out.println("Program By Sid Ahmed Mohamed")
+	}
 
 }
